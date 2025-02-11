e357d0ac9c35c5f67bf7b095088d5e57500e9ed0
shawn graham <sgraham5594@gmail.com>
2025-02-11 16:07:42 -0700

    route change

--- Assignment2/components/sign-in.tsx
+++ Assignment2/components/sign-in.tsx
@@ -8,6 +8,8 @@ import {
     Alert,
 } from "react-native";
 import credentials from "../credentials.json";
+import { useRouter } from "expo-router";  // Use the useRouter hook correctly
+
 
 // Define the props for the SignIn component
 type SignInProps = {
@@ -40,7 +42,7 @@ const SignIn: React.FC<SignInProps> = ({
             hasSpecialChar
         );
     };
-
+    const router = useRouter();
     // Function to handle login logic
     const handleLogin = () => {
         if (username.length < 5) {
@@ -69,6 +71,8 @@ const SignIn: React.FC<SignInProps> = ({
         }
 
         setIsSignedIn(true);
+        router.push('/tabs');
+
     };
 
     return (
